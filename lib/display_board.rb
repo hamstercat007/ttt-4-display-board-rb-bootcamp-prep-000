a3daf8e40bcf57d6c9e224c49fa8b8acd6d8b9ee
Lisa Mac <lisamac421@gmail.com>
2018-08-01 19:33:11 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,11 +5,11 @@ board = ["X","O"," "," "," "," "," "," "," "]
 
 def display_board(board)
   line = "-----------"
-  puts  " #{board[0]} |  #{board[1]} |  #{board[2]} "
+  puts  " #{board[0]} | #{board[1]} | #{board[2]} "
   puts  line
-  print " #{board[3]} |  #{board[4]} |  #{board[5]} "
+  print " #{board[3]} | #{board[4]} | #{board[5]} "
   print line
-  print " #{board[6]} |  #{board[7]} |  #{board[8]} "
+  print " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 display_board(board)
