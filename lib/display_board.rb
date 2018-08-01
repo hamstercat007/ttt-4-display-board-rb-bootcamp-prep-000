806e9d120637f9158172c72f5f16066eb1d193e6
Lisa Mac <lisamac421@gmail.com>
2018-08-01 19:38:38 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,8 +7,8 @@ def display_board(board)
   puts  " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  print "-----------"
-  print " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 display_board(board)
