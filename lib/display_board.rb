9d00ce7368a578fa227c59d531a36f4aa9be6d60
Lisa Mac <lisamac421@gmail.com>
2018-08-01 19:34:41 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-board = ["X","O"," "," "," "," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
   line = "-----------"
