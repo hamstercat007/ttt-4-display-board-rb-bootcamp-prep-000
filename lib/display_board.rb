4e10c843479e8178fd2788d25ca38abc21c63459
Lisa Mac <lisamac421@gmail.com>
2018-08-01 19:39:09 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,10 +4,11 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
+  line = "-----------"
   puts  " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
+  puts line
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
+  puts line
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
